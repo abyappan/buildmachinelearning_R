9fe6121383dcae8c37e6039f1a754610c2f4964a
abyappan <anjali.byappanahalli@osumc.edu>
2023-07-06 16:08:32 +0000

    update

--- R_lm.r
+++ R_lm.r
@@ -8,6 +8,21 @@ library(ggplot2)
 ggplot() +
   geom_point(aes(x = dataset$x, y = dataset$y),
              colour = 'red') +
+  ggtitle('x vs y') +
+  xlab('x') +
+  ylab('y')
+
+library(ggplot2)
+ggplot() +
+  geom_line(aes(x = dataset$x, y = predict(model, newdata = dataset)),
+            colour = 'blue') +
+  ggtitle('x vs y') +
+  xlab('x') +
+  ylab('y')
+
+library(ggplot2)
+ggplot() + geom_point(aes(x = dataset$x, y = dataset$y),
+             colour = 'red') +
   geom_line(aes(x = dataset$x, y = predict(model, newdata = dataset)),
             colour = 'blue') +
   ggtitle('x vs y') +
