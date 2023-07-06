6f36ca0dbcc02aa5e7f4c89742923e0241edfd73
abyappan <anjali.byappanahalli@osumc.edu>
2023-07-06 16:10:32 +0000

    update

--- R_lm.r
+++ R_lm.r
@@ -1,9 +1,12 @@
+# Set file name variable
 dataset = read.csv('regrex1.csv')
 
+# Linear model and summary
 model = lm(formula = y ~ x, data = dataset)
 
 summary(model)
 
+# Plot scatter plot of original data
 library(ggplot2)
 ggplot() +
   geom_point(aes(x = dataset$x, y = dataset$y),
@@ -12,6 +15,7 @@ ggplot() +
   xlab('x') +
   ylab('y')
 
+# Plot linear regression mode
 library(ggplot2)
 ggplot() +
   geom_line(aes(x = dataset$x, y = predict(model, newdata = dataset)),
@@ -20,6 +24,7 @@ ggplot() +
   xlab('x') +
   ylab('y')
 
+# Plot linear regression model with original data
 library(ggplot2)
 ggplot() + geom_point(aes(x = dataset$x, y = dataset$y),
              colour = 'red') +
