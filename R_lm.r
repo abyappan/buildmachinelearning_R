20e4c0385715e521a918a0c7f2f186051bcb05d4
abyappan <anjali.byappanahalli@osumc.edu>
2023-07-06 16:07:01 +0000

    update

---
+++ R_lm.r
@@ -0,0 +1,15 @@
+dataset = read.csv('regrex1.csv')
+
+model = lm(formula = y ~ x, data = dataset)
+
+summary(model)
+
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = dataset$x, y = dataset$y),
+             colour = 'red') +
+  geom_line(aes(x = dataset$x, y = predict(model, newdata = dataset)),
+            colour = 'blue') +
+  ggtitle('x vs y') +
+  xlab('x') +
+  ylab('y')
